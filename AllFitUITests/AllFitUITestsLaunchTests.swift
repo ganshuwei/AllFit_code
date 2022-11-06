a012190de5246a11ec7bee0c800fc861b5eb2197
Shuwei Gan <Shuwei Gan>
2022-11-05 19:05:39 -0500

    Initial Commit

---
+++ AllFitUITests/AllFitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AllFitUITestsLaunchTests.swift
+//  AllFitUITests
+//
+//  Created by 甘书玮 on 2022/11/5.
+//
+
+import XCTest
+
+class AllFitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
